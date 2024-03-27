f0f0f8b6f28e223c56b4e682177d67e3cb3f83e7
Ivlad34 <127123191+Ivlad34@users.noreply.github.com>
2024-03-27 21:51:48 +0300

    Add files via upload

---
+++ Sr2703.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    int k = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int y = 0; y < n - i; y++) {
+            cout << " ";
+        }
+        for (int z = 0; z < i + k; z++) {
+            cout << "*";
+        }
+        cout << "\n";
+        k++;
+    }
+}
+
